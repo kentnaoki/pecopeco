a711d005ffe704abd4767fe2c9ab3a05229bc3d5
naoki <ueda.191201@gmail.com>
2022-12-06 04:53:42 +0000

    added footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,70 @@
+import "./Footer.css";
+
 export const Footer = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <footer
+      id="footer-section"
+      style={{
+        background: `url(
+          "https://markdoor.net/pecopecopenguin/wp-content/uploads/2022/11/スクリーンショット-2022-11-19-235629.png"
+        )`,
+        backgroundSize: `cover`,
+      }}
+    >
+      <div className="footer-content">
+        <div className="footer-main">
+          <div>
+            <div className="icon">
+              <a href="./">
+                <img src="https://markdoor.net/pecopecopenguin/wp-content/plugins/burger-companion/inc/appetizer/images/icon_gif/footer-above-info/avatar-calm-approved.gif" />
+              </a>
+            </div>
+            <div className="footer-info">
+              <a href="./">Privacy Policy</a>
+            </div>
+          </div>
+          <div>
+            <div className="icon">
+              <a href="./">
+                <img
+                  src="https://markdoor.net/pecopecopenguin/wp-content/plugins/burger-companion/inc/appetizer/images/icon_gif/footer-above-info/shield-security.gif"
+                  alt="利用規約"
+                />
+              </a>
+            </div>
+            <div className="footer-info">
+              <a href="./">利用規約</a>
+            </div>
+          </div>
+          <div>
+            <div className="icon">
+              <a href="./">
+                <img
+                  src="https://markdoor.net/pecopecopenguin/wp-content/plugins/burger-companion/inc/appetizer/images/icon_gif/footer-above-info/gift.gif"
+                  alt="利用規特定商取引法に基づく表記"
+                />
+              </a>
+            </div>
+            <div className="footer-info">
+              <a href="./">利用規約・特定商取引法に基づく表記</a>
+            </div>
+          </div>
+          <div>
+            <div className="icon">
+              <a href="./">
+                <img
+                  src="https://markdoor.net/pecopecopenguin/wp-content/uploads/2022/11/小見出しを追加-300x300.png"
+                  alt="運営会社"
+                />
+              </a>
+            </div>
+            <div className="footer-info">
+              <a href="./">運営会社 Mark Doorについて</a>
+            </div>
+          </div>
+        </div>
+        <div className="footer-copyright"></div>
+      </div>
+    </footer>
   );
 };
