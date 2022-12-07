432ac588db643d7023c78b550db2c7fe8d287ae4
naoki <ueda.191201@gmail.com>
2022-12-07 21:33:38 +0000

    added button to connect metamask

--- src/BreadcrumbSection.jsx
+++ src/BreadcrumbSection.jsx
@@ -1,7 +1,7 @@
 import "./BreadcrumbSection.css";
 import "./Header.css";
 
-export const BreadcrumbSection = () => {
+export const BreadcrumbSection = (props) => {
   return (
     <div>
       <section
@@ -11,12 +11,22 @@ export const BreadcrumbSection = () => {
           backgroundImage: `url("https://markdoor.net/pecopecopenguin/wp-content/uploads/2022/11/スクリーンショット-2022-11-19-235227.png")`,
         }}
       >
-        <div className="container">
-          <div className="breadcrumb-heading">
-            <div className="heading-text-container">
-              <div>- Peco Peco Penguin -</div>
-              <div>食の三味一体を彦摩呂が実現</div>
-              <div>三方よしの食コミュニティを目指します</div>
+        <div className="background-black">
+          <div className="container">
+            <div className="breadcrumb-heading">
+              <div className="heading-text-container">
+                <div>- Peco Peco Penguin -</div>
+                <div>食の三味一体を彦摩呂が実現</div>
+                <div>三方よしの食コミュニティを目指します</div>
+              </div>
+              <div className="connect-wallet">
+                <button
+                  onClick={props.handleClick}
+                  className="connect-wallet-button"
+                >
+                  connect wallet
+                </button>
+              </div>
             </div>
           </div>
         </div>
