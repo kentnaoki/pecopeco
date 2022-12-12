752e28ae6110b41dc344626c43a11c2c5fd15602
naoki <ueda.191201@gmail.com>
2022-12-12 22:53:52 +0000

    made footerItems component and simplified the html code

---
+++ src/FooterItems.jsx
@@ -0,0 +1,27 @@
+import privacyPolicyImage from "./img/privacyPolicy.gif";
+import shieldSecurityImage from "./img/shieldSecurity.gif";
+import giftImage from "./img/gift.gif";
+import markdoorImage from "./img/markdoor.png";
+
+export const footerItems = [
+  {
+    id: 0,
+    text: "Privacy Policy",
+    img: privacyPolicyImage,
+  },
+  {
+    id: 1,
+    text: "利用規約",
+    img: shieldSecurityImage,
+  },
+  {
+    id: 2,
+    text: "利用規約・特定商取引法に基づく表記",
+    img: giftImage,
+  },
+  {
+    id: 3,
+    text: "運営会社 Mark Doorについて",
+    img: markdoorImage,
+  },
+];
