e011d3bcef8ad9050e43dc47fb3cc1bd43615004
naoki <ueda.191201@gmail.com>
2022-12-14 02:25:47 +0000

    changed footer style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,45 +1,102 @@
-import "./Footer.css";
+import styled from "styled-components";
 import { footerItems } from "./FooterItems";
+import email from "./img/email.png";
+
+const FooterStyle = styled.div`
+  .footer-section {
+    background-color: black;
+    color: #999999;
+  }
+  .footer-section a {
+    color: #999999;
+    text-decoration: none;
+    display: flex;
+  }
+  .footer-section a:hover {
+    text-decoration: underline;
+    color: #745801;
+    cursor: pointer;
+  }
+  .footer-content {
+    padding-top: 50px;
+    padding-bottom: 20px;
+  }
+  .footer-main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+  .row-content {
+    display: flex;
+    align-items: center;
+    background-color: #303030;
+  }
+  .icon img {
+    max-width: 50px;
+    max-height: auto;
+    padding: 10px;
+  }
+  .footer-info {
+    padding-right: 10px;
+  }
+  .contact {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem;
+    gap: 5px;
+  }
+  .contact-img-container {
+    display: flex;
+  }
+  .contact-img-container img {
+    max-width: 30px;
+    max-height: auto;
+  }
+  .copyright {
+    text-align: center;
+  }
+`;
 
 export const Footer = () => {
   return (
-    <footer
-      id="footer-section"
-      style={{
-        background: `url(
-          "https://markdoor.net/pecopecopenguin/wp-content/uploads/2022/11/スクリーンショット-2022-11-19-235629.png"
-        )`,
-        backgroundSize: `cover`,
-      }}
-    >
-      <div className="footer-content">
-        <div className="footer-main">
-          {footerItems.map((e) => {
-            return (
-              <div className="row-content" key={e.id}>
-                <div className="icon">
-                  <a href="./">
-                    <img src={e.img} />
-                  </a>
+    <FooterStyle>
+      <footer className="footer-section">
+        <div className="footer-content">
+          <div className="footer-main">
+            {footerItems.map((e) => {
+              return (
+                <div className="row-content" key={e.id}>
+                  <div className="icon">
+                    <a href="./">
+                      <img src={e.img} />
+                    </a>
+                  </div>
+                  <div className="footer-info">
+                    <a href="./">{e.text}</a>
+                  </div>
                 </div>
-                <div className="footer-info">
-                  <a href="./">{e.text}</a>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="footer-copyright">
-          <div className="copyright">
-            Copyright © 2022 Peco Peco Penguin |<br></br> Powered by Appetizer
+              );
+            })}
           </div>
-          <div className="contact">
-            <div>
-              <a href="./">お問い合わせ</a>
+          <div className="footer-copyright">
+            <div className="contact">
+              <div className="contact-img-container">
+                <a>
+                  <img src={email} />
+                </a>
+              </div>
+              <div>
+                <a href="./">お問い合わせ</a>
+              </div>
+            </div>
+            <div className="copyright">
+              Copyright © 2022 Peco Peco Penguin | Powered by Appetizer
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </FooterStyle>
   );
 };
