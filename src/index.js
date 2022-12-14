3920426c501c58c201b637975f4c4f446c1509eb
naoki <ueda.191201@gmail.com>
2022-12-14 02:55:27 +0000

    added appid to moralis

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { MoralisProvider } from "react-moralis";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MoralisProvider>
+    <MoralisProvider appId="pecopecopenguin">
       <App />
     </MoralisProvider>
   </React.StrictMode>
